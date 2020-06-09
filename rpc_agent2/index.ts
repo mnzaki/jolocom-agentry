3cce4e0814383008aa80dc8b959b225786e3cb7c
mnzaki <mnzaki@gmail.com>
2020-06-09 22:23:24 +0200

    WIP

--- rpc_agent2/index.ts
+++ rpc_agent2/index.ts
@@ -1,8 +1,10 @@
 const { JSONWebToken } = require('jolocom-lib/js/interactionTokens/JSONWebToken')
 const typeorm = require('typeorm')
 const { JolocomTypeormStorage } = require('@jolocom/sdk-storage-typeorm')
-const { JolocomSDK, FilePasswordStore } = require('@jolocom/sdk')
-const WebSocket = require('ws')
+const { JolocomSDK } = require('@jolocom/sdk')
+const { isEncryptionRequest, isDecryptionRequest } = require('@jolocom/sdk/js/src/lib/interactionManager/guards')
+import { FilePasswordStore } from '@jolocom/sdk-password-store-filesystem'
+import * as WebSocket from 'ws'
 let initialJWT
 
 const typeormConfig = {
@@ -49,31 +51,39 @@ async function start() {
   const wsURL = token.payload.interactionToken.callbackURL
   console.log('payload is here, need to get the ws:// url out!', token.payload, wsURL)
 
-  const ws = new WebSocket(wsURL, { perMessageDeflate: false })
+  const ws = new WebSocket(wsURL)
 
   let ready = false
   ws.on('open', function open() {
     console.log('Websocket opened to', wsURL)
     consumeAuthToken(sdk, initialJWT).then(response => {
       console.log('response', response)
-      ws.send(response)
+      ws.send(JSON.stringify(response))
     })
-    ready = true
-    console.log('READY!')
   });
 
   ws.on('message', function incoming(message) {
-    console.log('received message', message)
+    if (!ready) {
+      ready = true
+      console.log('READY!')
+      //ws.send(JSON.stringify({}))
+      return
+    }
+    console.log('received message:', message)
+    if (!message.trim()) {
+      console.log('no payload')
+      return
+    }
     const token = JSONWebToken.decode(message)
-    console.log('decoded to token: %s', token);
+    console.log('decoded to token:', token.nonce);
     if (!ready) {
       console.log('discarding message because not ready')
       return
     } else {
       console.log('consuming RPC')
       consumeRPCToken(sdk, message).then(result => {
-        ws.send(result)
         console.log('sent result', result)
+        ws.send(JSON.stringify(result))
       })
     }
   })
@@ -86,13 +96,26 @@ async function consumeAuthToken(sdk, jwt) {
   // see sdk/sso/authenticationRequest
   const token = JSONWebToken.decode(jwt)
   const { interactionManager } = sdk.bemw
-  const interaction = await interactionManager.start(
-    '', // channel,
-    token
-  )
-  const response = await interaction.createAuthenticationResponse()
-  // FIXME this doesn't work because new transport layer WS
-  // interaction.send(response)
+  let interaction
+  try {
+    interaction = await interactionManager.start(
+      '', // channel,
+      token
+    )
+  } catch (err) {
+    console.error('failed to start interaction')
+    throw err
+  }
+
+  let response
+  try {
+    response = await interaction.createAuthenticationResponse()
+    // FIXME this doesn't work because new transport layer WS
+    // interaction.send(response)
+  } catch (err) {
+    console.error('failed to createAuthenticationResponse')
+    throw err
+  }
 
   return response.encode()
 }
@@ -100,11 +123,24 @@ async function consumeAuthToken(sdk, jwt) {
 async function consumeRPCToken(sdk, jwt) {
   // see sdk/sso/authenticationRequest
   const token = JSONWebToken.decode(jwt)
-  sdk.tokenRecieved(jwt)
+  const weGood = await sdk.tokenRecieved(jwt)
+  if (!weGood) {
+    throw new Error('could not process token: ' + jwt)
+  }
+
   const { interactionManager } = sdk.bemw
   const interaction = interactionManager.getInteraction(token.nonce)
-  const response = await interaction.createDecResponseToken()
-  const res = Buffer.from(response.body.result, 'base64').toString()
+  const rpcToken = token.payload.interactionToken
 
-  return res
+  let respToken
+  if (isEncryptionRequest(rpcToken)) {
+    respToken = await interaction.createEncResponseToken()
+  } else if (isDecryptionRequest(rpcToken)) {
+    debugger
+    respToken = await interaction.createDecResponseToken()
+  } else {
+    throw new Error('of course')
+  }
+
+  return respToken.encode()
 }
